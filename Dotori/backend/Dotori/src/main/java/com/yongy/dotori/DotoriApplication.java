75ae6c84080b84f96d6e7c8fc2a7eb0ca6682bc1
Poposong <psi1908@naver.com>
2023-09-07 18:21:29 +0900

    #S09P22D107-66 #in-progress Feat(User) : 환경설정, 폴더 설정

--- Dotori/backend/Dotori/src/main/java/com/yongy/dotori/DotoriApplication.java
+++ Dotori/backend/Dotori/src/main/java/com/yongy/dotori/DotoriApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DotoriApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DotoriApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DotoriApplication.class, args);
+    }
 
 }
--- Dotori/backend/Dotori/src/test/java/com/yongy/dotori/DotoriApplicationTests.java
+++ Dotori/backend/Dotori/src/test/java/com/yongy/dotori/DotoriApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DotoriApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
