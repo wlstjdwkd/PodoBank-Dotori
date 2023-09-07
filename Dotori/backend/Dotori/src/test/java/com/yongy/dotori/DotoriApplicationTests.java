7184f731545a25786d09b1ddf44af609d6dc7546
Poposong <psi1908@naver.com>
2023-09-07 17:49:57 +0900

    #S09P22D107-66 #in-progress Feat(User) : 환경설정, 폴더 설정

---
+++ Dotori/backend/Dotori/src/test/java/com/yongy/dotori/DotoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yongy.dotori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
