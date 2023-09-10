95d59e2188a8467c72fbab50f514838b639115be
psi1908 <psi1908@naver.com>
2023-09-11 01:00:57 +0900

    #S09P22D107-93 #in-progress Feat(Security) : SecurityConfig 파일 생성하고 JWT 진행중

--- Dotori/backend/Dotori/src/main/java/com/yongy/dotori/global/security/SecurityConfig.java
+++ Dotori/backend/Dotori/src/main/java/com/yongy/dotori/global/security/SecurityConfig.java
@@ -1,36 +1,36 @@
-//package com.yongy.dotori.global.security;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//@Configuration
-//public class SecurityConfig {
-//    private final SecurityUserService service;
-//    private final JwtTokenFilter filter;
-//    private final JwtTokenProvider provide;
-//
-//    // 비밀번호를 DB에 저장하기 전 사용할 암호화
-//    @Bean
-//    PasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//
-//    public SecurityConfig(SecurityUserService service, JwtTokenFilter filter, JwtTokenProvider provide){
-//        this.service = service;
-//        this.filter = filter;
-//        this.provide = provide;
-//    }
-//
-//
-//    // css, js, 일반 접근(/)에 대해서는 시큐리티가 검사하지 않고 허용하겠다는 의미이다.
-//    @Bean
-//    WebSecurityCustomizer webSecurityCustomizer(){
-//        return (web) -> web.ignoring().anyRequest().requestMatchers("/css/**","/js/**","/img/**","/");
-//    }
-//
-//
-//}
+package com.yongy.dotori.global.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+    private final SecurityUserService service;
+    private final JwtTokenFilter filter;
+    private final JwtTokenProvider provide;
+
+    // 비밀번호를 DB에 저장하기 전 사용할 암호화
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+    public SecurityConfig(SecurityUserService service, JwtTokenFilter filter, JwtTokenProvider provide){
+        this.service = service;
+        this.filter = filter;
+        this.provide = provide;
+    }
+
+
+    // css, js, 일반 접근(/)에 대해서는 시큐리티가 검사하지 않고 허용하겠다는 의미이다.
+    @Bean
+    WebSecurityCustomizer webSecurityCustomizer(){
+        return (web) -> web.ignoring().anyRequest().requestMatchers("/css/**","/js/**","/img/**","/");
+    }
+
+
+}
