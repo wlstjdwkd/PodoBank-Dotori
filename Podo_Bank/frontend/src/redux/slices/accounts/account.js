398748cd20188bf4185b7a892b45d2cfc9945ea1
kimbeomgi <dyd1308@naver.com>
2023-09-12 10:41:51 +0900

    #S09P22D107-193 #done #comment fix(All):Redux-toolkit이용 및 axios 이용을 위한 directory 구조 변경

---
+++ Podo_Bank/frontend/src/redux/slices/accounts/account.js
@@ -0,0 +1,55 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+// 슬라이스(slice) 리듀서 정의
+const accountSlice = createSlice({
+  name: 'account',
+  initialState: {
+    count: 0,
+    nameorigin: null,
+    nametmp:null,
+  },
+  reducers: {
+    increment(state) {
+      state.count += 1;
+      state.nameorigin = 'kim';
+      state.nametmp = state.nameorigin;
+    },
+    decrement(state) {
+      state.count -= 1;
+      state.nameorigin ='no';
+      state.nametmp = state.nameorigin;
+    },
+    incrementByAmount(state, action) {
+      state.count += action.payload;
+      state.nameorigin = 'what';
+      state.nametmp = state.nameorigin;
+    },
+    changeNameNum(state) {
+      const randomNum = Math.floor(Math.random() * 100) +1;
+      state.nametmp = `${state.nameorigin}${randomNum}`;
+    },
+  },
+});
+
+export const { increment, decrement, incrementByAmount, changeNameNum } =
+accountSlice.actions;
+
+export default accountSlice.reducer;
+
+
+
+
+// // 사용 예시 
+// import { useSelector, useDispatch } from 'react-redux';
+// import { decrement, increment, incrementByAmount,changeNameNum } from '../../redux/slices/accounts/account'
+// const count = useSelector((state) => state.counter.count);
+// const name = useSelector((state) => state.counter.nametmp);
+// const count2 = useSelector((state) => state.whole.count2);
+// const name2 = useSelector((state) => state.whole.nameTmp2);
+// const dispatch = useDispatch();
+// <Button
+//   title="Increment"
+//   aria-label="Increment value"
+//   onPress={() => dispatch(increment())}
+// />
