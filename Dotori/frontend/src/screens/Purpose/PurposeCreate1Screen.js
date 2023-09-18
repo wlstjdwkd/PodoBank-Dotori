2276c2acbef5b959959aba71c8f95c438ac987b7
BangJinSeong <xkakrlfh@naver.com>
2023-09-18 17:38:53 +0900

    #S09P22D107-262 #in-progress #feat(Purpose) : 목표 생성 관련 화면 구성

---
+++ Dotori/frontend/src/screens/Purpose/PurposeCreate1Screen.js
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from "react-native";
+
+import HeaderComponent from "../Components/HeaderScreen";
+
+export default function PurposeCreate1Screen({ navigation }) {
+  const [purposeInfo, setPurposeInfo] = useState({
+    purposeName: "",
+  });
+  const [isValid, setIsValid] = useState(false);
+  const handleNameChange = (text) => {
+    setPurposeInfo((prev) => ({ ...prev, purposeName: text }));
+  };
+  return (
+    <View style={styles.container}>
+      <HeaderComponent
+        title="목표생성(1/3)"
+        cancelNavi="PurposeScreen"
+        navigation={navigation}
+      ></HeaderComponent>
+      <View style={styles.header}>
+        <Text style={styles.title}>목표 설정하기</Text>
+        <Text style={styles.subtitle}>달성할 목표를 입력해주세요.</Text>
+        <View style={styles.imageView}>
+          <Image
+            style={styles.middleImage}
+            source={require("../../assets/images/Hamster/PurposeCreateHamster.png")}
+          ></Image>
+        </View>
+
+        <TextInput
+          style={styles.input}
+          onChangeText={handleNameChange}
+          multiline={true}
+        />
+        <View style={styles.textRight}>
+          <Text style={styles.instructionText}>10자 이내로 작성해주세요.</Text>
+        </View>
+      </View>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() =>
+          navigation.navigate("PurposeCreate2Screen", {
+            purposeInfo: purposeInfo,
+          })
+        }
+        //TODO: 풀기
+        // disabled={!isValid}
+      >
+        <Text style={styles.buttonText}>다음</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    padding: 20,
+    backgroundColor: "white",
+  },
+  header: {
+    flex: 1,
+    // justifyContent: "center",
+    // alignItems: "center",
+    marginTop: 90,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  subtitle: {
+    color: "#7B7B7B",
+    marginBottom: 30,
+  },
+  input: {
+    width: "100%",
+    height: 50,
+    backgroundColor: "#D9D9D920",
+    borderWidth: 1,
+    borderColor: "#BAC0CA",
+    borderRadius: 10,
+    padding: 10,
+    // textAlign: "center",
+  },
+  button: {
+    height: 50,
+    backgroundColor: "#FF965C",
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+  },
+  imageView: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 30,
+    marginBottom: 40,
+  },
+  middleImage: {
+    // justifyContent: "center",
+    // alignItems: "center",
+    width: 150,
+    height: 150,
+  },
+  textRight: {
+    alignItems: "flex-end",
+  },
+  instructionText: {
+    color: "#A9A9A9",
+  },
+});
---
+++ Dotori/frontend/src/screens/Purpose/PurposeScreen.js
@@ -0,0 +1,204 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import FooterScreen from "../Components/FooterScreen";
+export default function PurposeScreen({ navigation }) {
+  const [nowPurposeAmount, setNowPurposeAmount] = useState(3000000);
+  // 여기서 목표 데이터를 추가하세요
+  const data = [
+    {
+      id: "1",
+      name: "목표1",
+      currentAmount: 2500,
+      targetAmount: 10000,
+    },
+    {
+      id: "2",
+      name: "목표1",
+      currentAmount: 2500,
+      targetAmount: 10000,
+    },
+    // 다른 목표들...
+  ];
+
+  const formatNumber = (num) => {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* 상단 이미지 */}
+      <Image
+        style={styles.topImage}
+        source={require("../../assets/images/dotori_logo.png")}
+      />
+
+      {/* 최신순 & 저축액 */}
+      <View style={styles.dateAmountContainer}>
+        <Text style={styles.latest}>최신순</Text>
+        <View style={styles.colContainer}>
+          <Text style={styles.amount}>{formatNumber(nowPurposeAmount)}원</Text>
+          <Text style={styles.currentSavings}>현재 저축액</Text>
+        </View>
+      </View>
+
+      {/* 중간 이미지 */}
+      <View style={styles.middleImageContainer}>
+        <Image
+          style={styles.middleImage}
+          source={require("../../assets/images/Hamster/PurposeHamster.png")}
+        />
+      </View>
+
+      <View style={styles.divider}></View>
+
+      {/* 목표 리스트 */}
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <View style={styles.targetContainer}>
+            <Text style={styles.targetName}>{item.name}</Text>
+            <View style={styles.rightAlignContainer}>
+              <Text style={styles.currentAmount}>
+                {formatNumber(item.currentAmount)}원
+              </Text>
+            </View>
+            <View style={styles.targetAmounts}>
+              <Text style={styles.targetAmountText}>목표 금액</Text>
+              <View style={styles.rightAlignContainer}>
+                <Text style={styles.targetAmount}>
+                  {formatNumber(item.targetAmount)}원
+                </Text>
+              </View>
+            </View>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+        ListFooterComponent={
+          <TouchableOpacity
+            style={styles.addButton}
+            onPress={() => navigation.navigate("PurposeCreate1Screen")}
+          >
+            <Text style={styles.addText}>+</Text>
+          </TouchableOpacity>
+        }
+      />
+      <View style={styles.footer}>
+        <FooterScreen navigation={navigation} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    paddingHorizontal: 20,
+  },
+  topImage: {
+    alignSelf: "center",
+    marginTop: 20,
+    resizeMode: "contain",
+    width: 70,
+    height: 70,
+  },
+  dateAmountContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  latest: {
+    // 여기에 텍스트 스타일 추가...
+    backgroundColor: "#EFF3F6",
+    borderRadius: 10,
+    fontSize: 12,
+    padding: 10,
+  },
+  amount: {
+    // 여기에 텍스트 스타일 추가...
+    fontSize: 20,
+  },
+  currentSavings: {
+    color: "#9D9D9D",
+    // 여기에 추가 스타일...
+  },
+  middleImageContainer: {
+    position: "absolute", // Set position to absolute
+    alignSelf: "center",
+    zIndex: 1, // Make sure the image is on top of the divider
+    top: "20%", // Adjust this to position the image correctly over the divider
+  },
+  middleImage: {
+    // 여기에 이미지 스타일 추가...
+    width: 100,
+    height: 110,
+  },
+  divider: {
+    marginTop: 115, // Height of the middleImage divided by 2, adjust this value as needed
+    height: 10,
+    backgroundColor: "#FFF2DE", // or any color you'd like
+    marginBottom: 30, // space after the divider
+  },
+  targetContainer: {
+    borderRadius: 15,
+    borderColor: "#A6A6A6",
+    borderWidth: 1,
+    padding: 10,
+    marginTop: 20,
+    flexDirection: "column",
+  },
+
+  targetName: {
+    marginBottom: 10,
+    fontSize: 20,
+    flex: 1,
+  },
+
+  rightAlignContainer: {
+    alignItems: "flex-end",
+    flex: 1,
+  },
+
+  currentAmount: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+
+  targetAmounts: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  targetAmountText: {
+    fontSize: 12,
+    flex: 1,
+  },
+
+  targetAmount: {
+    fontSize: 12,
+    textAlign: "right",
+    flex: 1,
+  },
+  addButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
+    borderColor: "#A6A6A6",
+    borderWidth: 1,
+    height: 70,
+    marginTop: 20,
+    borderStyle: "dashed", // 점선 테두리 추가
+  },
+  addText: {
+    color: "#A6A6A6",
+    fontSize: 24,
+  },
+});
