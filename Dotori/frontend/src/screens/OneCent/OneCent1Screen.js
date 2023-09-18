64850e90e96037a1a27eebfc7056bc871caf054e
BangJinSeong <xkakrlfh@naver.com>
2023-09-18 13:13:06 +0900

    #S09P22D107-252 #in-progress #feat(Signup) : 메인페이지

---
+++ Dotori/frontend/src/screens/OneCent/OneCent1Screen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+} from "react-native";
+
+export default function OneCent1Screen({ navigation }) {
+  return <View></View>;
+}
