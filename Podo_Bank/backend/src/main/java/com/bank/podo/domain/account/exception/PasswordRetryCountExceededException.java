05865b5a1d845559ea8ede3c52dc4d3703a6543e
hd9775 <kkang13277@naver.com>
2023-09-19 10:28:52 +0900

    #S09P22D107-279 #in-progress #Comment Fix(PodoBank) : 비밀번호 오류 메세지 수정

--- Podo_Bank/backend/src/main/java/com/bank/podo/domain/account/exception/PasswordRetryCountExceededException.java
+++ Podo_Bank/backend/src/main/java/com/bank/podo/domain/account/exception/PasswordRetryCountExceededException.java
@@ -1,7 +1,7 @@
 package com.bank.podo.domain.account.exception;
 
 public class PasswordRetryCountExceededException extends RuntimeException {
-    public PasswordRetryCountExceededException(String message) {
-        super(message);
+    public PasswordRetryCountExceededException(String message, int retryCount) {
+        super(message + " 3회 실패시 계좌가 잠김니다. (현재: " + retryCount + "회)");
     }
 }
