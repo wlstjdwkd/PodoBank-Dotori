3ce8b0042b8427cf927096936896ec4892ac09be
BangJinSeong <xkakrlfh@naver.com>
2023-09-21 09:23:21 +0900

    #S09P22D107-288 #done #feat(Plan) : 계획 생성 화면 관리

--- Dotori/frontend/src/screens/MyPage/CategorySettingScreen.js
+++ Dotori/frontend/src/screens/MyPage/CategorySettingScreen.js
@@ -1,176 +1,190 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image, FlatList } from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import HeaderComponent from "../Components/HeaderScreen";
 
 const categorys = [
-    {
-        categorySeq: '1',
-        categoryTitle: "옷(겨울)",
-        categoryList: ['마왕족발', '김치찜은 못참치', '최각루', '피자가좋다', '세찜', '치킨과피자가같이 이게', '내가닭이다'],
-        newCategoryList: ['신촌 등갈비찜']
-    },
-    {
-        categorySeq: '2',
-        categoryTitle: "요가학원",
-    },
-    {
-        categorySeq: '3',
-        categoryTitle: "가구구매",
-    },
-    {
-        categorySeq: '4',
-        categoryTitle: "식자재",
-    },
-    {
-        categorySeq: '5',
-        categoryTitle: "배달",
-    },
+  {
+    categorySeq: "1",
+    categoryTitle: "옷(겨울)",
+    categoryList: [
+      "마왕족발",
+      "김치찜은 못참치",
+      "최각루",
+      "피자가좋다",
+      "세찜",
+      "치킨과피자가같이 이게",
+      "내가닭이다",
+    ],
+    newCategoryList: ["신촌 등갈비찜"],
+  },
+  {
+    categorySeq: "2",
+    categoryTitle: "요가학원",
+  },
+  {
+    categorySeq: "3",
+    categoryTitle: "가구구매",
+  },
+  {
+    categorySeq: "4",
+    categoryTitle: "식자재",
+  },
+  {
+    categorySeq: "5",
+    categoryTitle: "배달",
+  },
 ];
 
 export default function CategorySettingScreen({ navigation, route }) {
-    // route.params를 통해 이전 페이지에서 받은 categorySeq를 가져옵니다.
-    const { categorySeq } = route.params;
+  // route.params를 통해 이전 페이지에서 받은 categorySeq를 가져옵니다.
+  const { categorySeq } = route.params;
 
-    // categorySeq와 일치하는 category 데이터를 찾습니다.
-    const category = categorys.find(item => item.categorySeq === categorySeq);
+  // categorySeq와 일치하는 category 데이터를 찾습니다.
+  const category = categorys.find((item) => item.categorySeq === categorySeq);
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                {/* 뒤로가기 버튼 */}
-                <TouchableOpacity onPress={() => navigation.goBack()}>
-                    <Image
-                        style={styles.arrowIcon}
-                        source={require('../../assets/icon/back_arrow.png')} // 화살표 아이콘 이미지 경로
-                    />
-                </TouchableOpacity>
-                
-            </View>
+  return (
+    <View style={styles.container}>
+      <HeaderComponent title="카테고리 세부 내용"></HeaderComponent>
 
-            <View style={styles.topContainer}>
-                {/* 페이지 상단 좌측에 categoryTitle 출력 */}
-                <View style={styles.categoryTitleContainer}>
-                    <Text style={styles.categoryTitle}>{category.categoryTitle}</Text>
-                    <Text style={styles.categoryTitleExplain}>자주 사용하는 사용처를 분류해주세요.</Text>
-                </View>
-                <Image
-                        style={styles.checkIcon}
-                        source={require('../../assets/images/Hamster/CheckHamster.png')}
-                    />
-            </View>    
-
-            {/* 자주 사용하는 사용처 */}
-            <View style={styles.categoryGroup}>
-                <Text style={styles.categoryGroupText}>자주 사용하는 사용처</Text>
-            </View>
-            {/* 카테고리 목록 */}
-            <FlatList
-                data={category.categoryList}
-                renderItem={({ item }) => (
-                    <View style={styles.categoryItem}>
-                        <Text style={styles.categoryItemText}>{item}</Text>
-                    </View>
-                )}
-                keyExtractor={(item, index) => index.toString()}
-                contentContainerStyle={styles.categoryList}
-                numColumns={3} // 2개의 열로 배치
-            />
-            {/* 구분선 */}
-            <View style={styles.separator}></View>
-            {/* 신규 사용처 */}
-            <View style={styles.categoryGroup}>
-                <Text style={styles.categoryGroupText}>신규 사용처</Text>
-            </View>
-            {/* 신규 카테고리 목록 */}
-            <FlatList
-                data={category.newCategoryList}
-                renderItem={({ item }) => (
-                    <View style={styles.categoryItem}>
-                        <Text style={styles.categoryItemText}>{item}</Text>
-                    </View>
-                )}
-                keyExtractor={(item, index) => index.toString()}
-                contentContainerStyle={styles.categoryList}
-                numColumns={2} // 2개의 열로 배치
-            />
+      <View style={styles.topContainer}>
+        {/* 페이지 상단 좌측에 categoryTitle 출력 */}
+        <View style={styles.categoryTitleContainer}>
+          <Text style={styles.categoryTitle}>{category.categoryTitle}</Text>
+          <Text style={styles.categoryTitleExplain}>
+            자주 사용하는 사용처를 분류해주세요.
+          </Text>
         </View>
-    );
+        <Image
+          style={styles.checkIcon}
+          source={require("../../assets/images/Hamster/CheckHamster.png")}
+        />
+      </View>
+
+      {/* 자주 사용하는 사용처 */}
+      <View style={styles.categoryGroup}>
+        <Text style={styles.categoryGroupText}>자주 사용하는 사용처</Text>
+      </View>
+      {/* 카테고리 목록 */}
+      <FlatList
+        data={category.categoryList}
+        renderItem={({ item }) => (
+          <View style={styles.categoryItem}>
+            <Text style={styles.categoryItemText}>{item}</Text>
+          </View>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        contentContainerStyle={styles.categoryList}
+        numColumns={3} // 2개의 열로 배치
+      />
+      {/* 구분선 */}
+      <View style={styles.separator}></View>
+      {/* 신규 사용처 */}
+      <View style={styles.categoryGroup}>
+        <Text style={styles.categoryGroupText}>신규 사용처</Text>
+      </View>
+      {/* 신규 카테고리 목록 */}
+      <FlatList
+        data={category.newCategoryList}
+        renderItem={({ item }) => (
+          <View style={styles.categoryItem}>
+            <Text style={styles.categoryItemText}>{item}</Text>
+          </View>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        contentContainerStyle={styles.categoryList}
+        numColumns={2} // 2개의 열로 배치
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-        padding: 16,
-    },
-    header: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: 16,
-        marginTop: 15,
-    },
-    arrowIcon: {
-        width: 20,
-        height: 20,
-    },
-    topContainer: {
-        flexDirection: 'row',
-        marginLeft: "5%",
-        marginTop: 20,
-        marginBottom: 50,
-    },
-    categoryTitleContainer: {
-        flexDirection: 'column',
-    },
-    categoryTitle: {
-        fontSize: 30,
-        flex: 1,
-        textAlign: 'left',
-        marginLeft: 16, // categoryTitle을 뒤에 추가
-        marginTop: 20,
-    },
-    categoryTitleExplain: {
-        fontSize: 10,
-        flex: 1,
-        textAlign: 'left',
-        marginLeft: 16, // categoryTitle을 뒤에 추가
-        color: "#7B7B7B"
-    },
-    categoryGroup: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: 10,
-    },
-    categoryGroupText: {
-        fontSize: 23,
-        fontWeight: 'bold',
-        marginLeft: '5%',
-        marginBottom: 10,
-    },
-    checkIcon: {
-        width: 150,
-        height: 150,
-        marginLeft: 10,
-    },
-    categoryList: {
-        flexDirection: 'column', // 세로로 배치
-        alignSelf: 'left'
-    },
-    categoryItem: {
-        alignItems: 'center',
-        padding: 2,
-        marginBottom: 8,
-        marginHorizontal: 8, // 좌우 여백 추가
-        borderWidth: 1, // 테두리 추가
-        borderColor: 'black', // 테두리 색상
-        borderRadius: 20, // 테두리 둥글게 설정
-    },
-    categoryItemText: {
-        fontSize: 16,
-        marginHorizontal: 5,
-    },
-    separator: {
-        height: 2,
-        backgroundColor: '#BAC0CA', // 구분선 색상 변경
-        marginVertical: 20,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 16,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 16,
+    marginTop: 15,
+  },
+  arrowIcon: {
+    width: 20,
+    height: 20,
+  },
+  topContainer: {
+    flexDirection: "row",
+    // alignContent: "place-between",
+    marginLeft: "5%",
+    marginTop: 20,
+    marginBottom: 50,
+  },
+  categoryTitleContainer: {
+    flexDirection: "column",
+  },
+  categoryTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+
+    flex: 1,
+    textAlign: "left",
+    marginLeft: 8, // categoryTitle을 뒤에 추가
+    marginTop: 70,
+    marginBottom: -10,
+  },
+  categoryTitleExplain: {
+    fontSize: 10,
+    flex: 1,
+    textAlign: "left",
+    marginLeft: 8, // categoryTitle을 뒤에 추가
+    color: "#7B7B7B",
+  },
+  categoryGroup: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  categoryGroupText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: "5%",
+    marginBottom: 10,
+  },
+  checkIcon: {
+    width: 150,
+    height: 150,
+    marginLeft: 10,
+  },
+  categoryList: {
+    flexDirection: "column", // 세로로 배치
+    alignSelf: "left",
+  },
+  categoryItem: {
+    alignItems: "center",
+    padding: 4,
+    marginBottom: 8,
+    marginHorizontal: 8, // 좌우 여백 추가
+    borderWidth: 1, // 테두리 추가
+    borderColor: "#7B7B7B", // 테두리 색상
+    borderRadius: 20, // 테두리 둥글게 설정
+  },
+  categoryItemText: {
+    fontSize: 16,
+    marginHorizontal: 5,
+  },
+  separator: {
+    height: 1,
+    backgroundColor: "#BAC0CA", // 구분선 색상 변경
+    marginVertical: 40,
+    marginTop: -100,
+  },
 });
--- Dotori/frontend/src/screens/MyPage/MyPageScreen.js
+++ Dotori/frontend/src/screens/MyPage/MyPageScreen.js
@@ -1,204 +1,211 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { AntDesign, Entypo, FontAwesome } from "@expo/vector-icons";
 
 const userInfo = {
-    name: '홍길동',
-    email: 'example@example.com',
-    birth: '2000-01-01',
-    phone: '010-1111-1111'
-}
+  name: "홍길동",
+  email: "example@example.com",
+  birth: "2000-01-01",
+  phone: "010-1111-1111",
+};
+import HeaderComponent from "../Components/HeaderScreen";
 
 export default function MyPageScreen({ navigation }) {
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                {/* 뒤로가기 버튼 */}
-                <TouchableOpacity onPress={() => navigation.goBack()}>
-                    <Image
-                        style={styles.arrowIcon}
-                        source={require('../../assets/icon/back_arrow.png')} // 화살표 아이콘 이미지 경로
-                    />
-                </TouchableOpacity>
+  return (
+    <View style={styles.container}>
+      <HeaderComponent title="마이페이지"></HeaderComponent>
 
-                {/* 마이페이지 글씨 */}
-                <Text style={styles.myPageText}>마이페이지</Text>
-            </View>
+      {/* 프로필 이미지 */}
+      <View style={styles.profileContainer}>
+        <Image
+          style={styles.profileImage}
+          source={require("../../assets/images/Hamster/MainHamster.png")} // 프로필 이미지 경로
+        />
+      </View>
 
-            {/* 프로필 이미지 */}
-            <View style={styles.profileContainer}>
-                <Image
-                    style={styles.profileImage}
-                    source={require('../../assets/images/Hamster/MainHamster.png')} // 프로필 이미지 경로
-                />
-            </View>
+      {/* 구분선 */}
+      <View style={styles.separator}></View>
 
-            {/* 구분선 */}
-            <View style={styles.separator}></View>
-
-            {/* 기본 정보 */}
-            <Text style={styles.boldText}>기본 정보</Text>
-            <View style={styles.infoItem}>
-                <Text style={styles.infoText}>이름</Text>
-                <Text style={styles.infoText}>{userInfo.name}</Text>
-            </View>
-            <View style={styles.infoItem}>
-                <Text style={styles.infoText}>이메일</Text>
-                <Text style={styles.infoText}>{userInfo.email}</Text>
-            </View>
-            <View style={styles.infoItem}>
-                <Text style={styles.infoText}>생년월일</Text>
-                <Text style={styles.infoText}>{userInfo.birth}</Text>
-            </View>
-            <View style={styles.infoItem}>
-                <Text style={styles.infoText}>핸드폰 번호</Text>
-                <View style={styles.infoRow}>
-                    <Text style={styles.infoText}>{userInfo.phone}</Text>
-                    <TouchableOpacity onPress={() => navigation.navigate('EditPhoneNumberScreen')}>
-                        <Image
-                            style={styles.editIcon}
-                            source={require('../../assets/icon/pencil.png')} // 편집 아이콘 이미지 경로
-                        />
-                    </TouchableOpacity>
-                </View>
-            </View>
-
-            {/* 구분선 */}
-            <View style={styles.separator}></View>
-
-            {/* 기능 항목 */}
-            <Text style={styles.boldText}>기능</Text>
-            <TouchableOpacity style={styles.featureItem}
-                 onPress={() => navigation.navigate("PasswordChangeScreen")}>
-                <Text style={styles.infoText}>비밀번호 변경</Text>
-                <Image
-                    style={styles.arrowIcon}
-                    source={require('../../assets/icon/forward_arrow.png')} // 화살표 아이콘 이미지 경로
-                />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.featureItem}
-                 onPress={() => navigation.navigate("CategoryScreen")}>
-                <Text style={styles.infoText}>카테고리 보기</Text>
-                <Image
-                    style={styles.arrowIcon}
-                    source={require('../../assets/icon/forward_arrow.png')} // 화살표 아이콘 이미지 경로
-                />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.featureItem}
-                 onPress={() => navigation.navigate("ReceipeSelectScreen")}>
-                <Text style={styles.infoText}>명세서 보기</Text>
-                <Image
-                    style={styles.arrowIcon}
-                    source={require('../../assets/icon/forward_arrow.png')} // 화살표 아이콘 이미지 경로
-                />
-            </TouchableOpacity>
-
-            {/* 로그아웃과 회원탈퇴 */}
-            <View style={styles.bottomRow}>
-                <TouchableOpacity style={styles.logoutButton}>
-                    <Text style={styles.logoutText}>로그아웃</Text>
-                </TouchableOpacity>
-                <Text style={styles.separatorText}>   </Text>
-                <TouchableOpacity style={styles.withdrawButton}>
-                    <Text style={styles.withdrawText}>회원탈퇴</Text>
-                </TouchableOpacity>
-            </View>
+      {/* 기본 정보 */}
+      <View style={styles.basicContainer}>
+        <Text style={styles.boldText}>기본 정보</Text>
+        <View style={styles.infoItem}>
+          <Text style={styles.infoText}>이름</Text>
+          <Text style={styles.infoText}>{userInfo.name}</Text>
         </View>
-    );
+        <View style={styles.infoItem}>
+          <Text style={styles.infoText}>이메일</Text>
+          <Text style={styles.infoText}>{userInfo.email}</Text>
+        </View>
+        <View style={styles.infoItem}>
+          <Text style={styles.infoText}>생년월일</Text>
+          <Text style={styles.infoText}>{userInfo.birth}</Text>
+        </View>
+        <View style={styles.infoItem}>
+          <Text style={styles.infoText}>핸드폰 번호</Text>
+          <View style={styles.infoRow}>
+            <Text style={styles.infoText}>{userInfo.phone}</Text>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("EditPhoneNumberScreen")}
+            >
+              <Image
+                style={styles.editIcon}
+                source={require("../../assets/icon/pencil.png")} // 편집 아이콘 이미지 경로
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+
+      {/* 구분선 */}
+      <View style={styles.separator}></View>
+
+      {/* 기능 항목 */}
+      {/* <Text style={styles.boldText}>기능</Text> */}
+      <View style={styles.basicContainer}>
+        <TouchableOpacity
+          style={styles.featureItem}
+          onPress={() => navigation.navigate("PasswordChangeScreen")}
+        >
+          <Text style={styles.infoText}>비밀번호 변경</Text>
+          <FontAwesome
+            name="angle-right"
+            style={{ fontSize: 30 }}
+          ></FontAwesome>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.featureItem}
+          onPress={() => navigation.navigate("CategoryScreen")}
+        >
+          <Text style={styles.infoText}>카테고리 보기</Text>
+          <FontAwesome
+            name="angle-right"
+            style={{ fontSize: 30 }}
+          ></FontAwesome>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.featureItem}
+          onPress={() => navigation.navigate("ReceipeSelectScreen")}
+        >
+          <Text style={styles.infoText}>명세서 보기</Text>
+          <FontAwesome
+            name="angle-right"
+            style={{ fontSize: 30 }}
+          ></FontAwesome>
+        </TouchableOpacity>
+      </View>
+
+      {/* 로그아웃과 회원탈퇴 */}
+      <View style={styles.bottomRow}>
+        <TouchableOpacity style={styles.logoutButton}>
+          <Text style={styles.logoutText}>로그아웃</Text>
+        </TouchableOpacity>
+        <Text style={styles.separatorText}> </Text>
+        <TouchableOpacity style={styles.withdrawButton}>
+          <Text style={styles.withdrawText}>회원탈퇴</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-        padding: 16,
-    },
-    header: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: 16,
-        marginTop: 15
-    },
-    arrowIcon: {
-        width: 20,
-        height: 20,
-    },
-    myPageText: {
-        fontSize: 24,
-        flex: 1, // 텍스트가 남은 공간을 모두 차지하도록 설정
-        textAlign: 'center',
-    },
-    profileContainer: {
-        alignItems: 'center',
-    },
-    profileImage: {
-        width: 100,
-        height: 100,
-        borderRadius: 50, // 원형 모양으로 만들기 위한 반지름 설정
-        marginVertical: 16,
-    },
-    separator: {
-        height: 3,
-        backgroundColor: '#F2EFEF',
-        marginVertical: 16,
-    },
-    boldText: {
-        fontWeight: 'bold',
-        fontSize: 25,
-        marginBottom: 8,
-    },
-    infoItem: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 8,
-        marginTop: 8,
-    },
-    infoText: {
-        fontSize: 15
-    },
-    infoRow: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    editIcon: {
-        width: 15,
-        height: 15,
-    },
-    featureItem: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 16,
-        marginTop: 10,
-    },
-    arrowIcon: {
-        width: 20,
-        height: 20,
-    },
-    bottomRow: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 16,
-    },
-    logoutButton: {
-        borderBottomWidth: 1,
-        borderBottomColor: '#3D3D3D',
-        paddingBottom: 2,
-    },
-    logoutText: {
-        fontSize: 10,
-    },
-    separatorText: {
-        marginHorizontal: 8,
-    },
-    withdrawButton: {
-        borderBottomWidth: 1,
-        borderBottomColor: '#FF3737',
-        paddingBottom: 2,
-    },
-    withdrawText: {
-        fontSize: 10,
-        color: '#FF3737',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 16,
+  },
+  basicContainer: {
+    paddingHorizontal: 16,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 16,
+    marginTop: 15,
+  },
+  arrowIcon: {
+    width: 20,
+    height: 20,
+  },
+  myPageText: {
+    fontSize: 24,
+    flex: 1, // 텍스트가 남은 공간을 모두 차지하도록 설정
+    textAlign: "center",
+  },
+  profileContainer: {
+    alignItems: "center",
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50, // 원형 모양으로 만들기 위한 반지름 설정
+    marginVertical: 16,
+  },
+  separator: {
+    height: 5,
+    backgroundColor: "#F2EFEF",
+    marginVertical: 16,
+  },
+  boldText: {
+    fontWeight: "bold",
+    fontSize: 20,
+
+    marginBottom: 16,
+  },
+  infoItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 8,
+    marginTop: 8,
+  },
+  infoText: {
+    fontSize: 15,
+  },
+  infoRow: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  editIcon: {
+    width: 15,
+    height: 15,
+  },
+  featureItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 16,
+    marginTop: 10,
+  },
+  arrowIcon: {
+    width: 20,
+    height: 20,
+  },
+  bottomRow: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 35,
+  },
+  logoutButton: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#3D3D3D",
+    paddingBottom: 2,
+  },
+  logoutText: {
+    fontSize: 10,
+  },
+  separatorText: {
+    marginHorizontal: 20,
+  },
+  withdrawButton: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#FF3737",
+    paddingBottom: 2,
+  },
+  withdrawText: {
+    fontSize: 10,
+    color: "#FF3737",
+  },
 });
