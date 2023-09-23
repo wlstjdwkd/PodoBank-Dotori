23530ac3e55835169826ea36820342f6cbeb8e5b
BangJinSeong <xkakrlfh@naver.com>
2023-09-23 14:14:40 +0900

    #S09P22D107-288 #done #feat(merge) : master merge

--- Dotori/backend/Dotori-User-Service/src/main/java/com/yongy/dotoriUserService/DotoriUserServiceApplication.java
+++ Dotori/backend/Dotori-User-Service/src/main/java/com/yongy/dotoriUserService/DotoriUserServiceApplication.java
@@ -2,8 +2,11 @@ package com.yongy.dotoriUserService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class DotoriUserServiceApplication {
 
 	public static void main(String[] args) {
