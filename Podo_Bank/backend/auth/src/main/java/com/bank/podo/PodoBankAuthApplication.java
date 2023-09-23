6c28e888705d0765e4ce963b1aa56ce5364a3c1c
서현덕 <kkang13277@naver.com>
2023-09-23 18:57:04 +0900

    #S09P22D107-306 #in-progress #Feat(podobank) : gateway 연결

--- Podo_Bank/backend/auth/src/main/java/com/bank/podo/PodoBankAuthApplication.java
+++ Podo_Bank/backend/auth/src/main/java/com/bank/podo/PodoBankAuthApplication.java
@@ -2,8 +2,10 @@ package com.bank.podo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PodoBankAuthApplication {
 
     public static void main(String[] args) {
