fa01e73ede347dc22f2bde3741f789a2bbb4e46a
hd9775 <kkang13277@naver.com>
2023-09-27 10:04:41 +0900

    #S09P22D107-382 #in-progress #Fix: 머지 오류 수정중

---
+++ Podo_Bank/backend/user/src/test/java/com/bank/podo/SsafyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.podo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafyBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
