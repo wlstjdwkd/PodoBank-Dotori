5ae7dfc03a0669777b3372d0efd5a41c2cff0751
hd9775 <kkang13277@naver.com>
2023-09-27 09:54:14 +0900

    #S09P22D107-382 #in-progress #Fix: 머지 오류 수정중

---
+++ Podo_Bank/backend/account/src/main/java/com/bank/podo/domain/account/controller/AccountAdminController.java
@@ -0,0 +1,38 @@
+package com.bank.podo.domain.account.controller;
+
+import com.bank.podo.domain.account.dto.*;
+import com.bank.podo.domain.account.service.AccountAdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/account/admin")
+public class AccountAdminController {
+    private final AccountAdminService accountAdminService;
+
+    @PostMapping("/transfer")
+    public ResponseEntity<Void> transfer(@RequestBody AdminTransferDTO transferDTO) {
+        accountAdminService.transfer(transferDTO);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/balance")
+    public ResponseEntity<AdminBalanceDTO> balance(@RequestBody AdminBalanceDTO balanceDTO) {
+        balanceDTO = accountAdminService.balance(balanceDTO);
+        return ResponseEntity.ok(balanceDTO);
+    }
+
+    @PostMapping("/history")
+    public ResponseEntity<List<TransactionHistoryDTO>> history(@RequestBody AdminHistoryDTO historyDTO) {
+        List<TransactionHistoryDTO> transactionHistoryDTO = accountAdminService.history(historyDTO);
+        return ResponseEntity.ok(transactionHistoryDTO);
+    }
+
+}
---
+++ Podo_Bank/backend/account/src/main/java/com/bank/podo/domain/account/entity/TransactionHistory.java
@@ -0,0 +1,56 @@
+package com.bank.podo.domain.account.entity;
+
+import com.bank.podo.domain.account.enums.TransactionType;
+import com.bank.podo.global.others.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "transaction_history",
+    indexes = {
+            @Index(name = "idx_account_seq", columnList = "account_seq"),
+            @Index(name = "idx_counter_account_seq", columnList = "counter_account_seq"),
+            @Index(name = "idx_create_at", columnList = "created_at")
+    }
+)
+public class TransactionHistory extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long transactionId;
+
+    @ManyToOne
+    @JoinColumn(name = "account_seq")
+    private Account account;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType transactionType;
+
+    @Column
+    private BigDecimal amount;
+
+    @Column
+    private BigDecimal balanceAfter;
+
+    @ManyToOne
+    @JoinColumn(name = "counter_account_seq")
+    private Account counterAccount;
+
+    @Column
+    private String content;
+
+    @Builder
+    public TransactionHistory(Account account, TransactionType transactionType, BigDecimal amount, BigDecimal balanceAfter, Account counterAccount, String content) {
+        this.account = account;
+        this.transactionType = transactionType;
+        this.amount = amount;
+        this.balanceAfter = balanceAfter;
+        this.counterAccount = counterAccount;
+        this.content = content;
+    }
+}
