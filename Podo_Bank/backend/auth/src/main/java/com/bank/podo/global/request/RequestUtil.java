d7d06d9a72894ccd8755bdbe0e790d097d3417bd
서현덕 <kkang13277@naver.com>
2023-10-01 15:48:40 +0900

    #S09P22D107-383 #in-progress #Fix(podobank) : 오타수정

--- Podo_Bank/backend/auth/src/main/java/com/bank/podo/global/request/RequestUtil.java
+++ Podo_Bank/backend/auth/src/main/java/com/bank/podo/global/request/RequestUtil.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class RequestUtil {
 
-    @Value("${http.reqeust.firebase.url}")
+    @Value("${http.request.firebase.url}")
     private String firebaseUrl;
 
     private final RequestApi requestApi;
