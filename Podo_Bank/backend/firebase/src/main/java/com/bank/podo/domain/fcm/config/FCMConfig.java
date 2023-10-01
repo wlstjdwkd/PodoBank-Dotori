ee1c80f701eb83a717dfbee268989e2993eb6903
서현덕 <kkang13277@naver.com>
2023-10-01 17:13:07 +0900

    #S09P22D107-383 #in-progress #Fix(podobank) : json -> properties로 수정

--- Podo_Bank/backend/firebase/src/main/java/com/bank/podo/domain/fcm/config/FCMConfig.java
+++ Podo_Bank/backend/firebase/src/main/java/com/bank/podo/domain/fcm/config/FCMConfig.java
@@ -4,26 +4,60 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @Configuration
 public class FCMConfig {
 
-    @Value("${firebase.config.path}")
-    private String FCM_PRIVATE_KEY_PATH;
+    @Value("${firebase.type}")
+    String type;
+
+    @Value("${firebase.project_id}")
+    String project_id;
+
+    @Value("${firebase.private_key_id}")
+    String private_key_id;
+
+    @Value("${firebase.private_key}")
+    String private_key;
+
+    @Value("${firebase.client_email}")
+    String client_email;
+
+    @Value("${firebase.client_id}")
+    String client_id;
+
+    @Value("${firebase.auth_uri}")
+    String auth_uri;
+
+    @Value("${firebase.token_uri}")
+    String token_uri;
+
+    @Value("${firebase.auth_provider_x509_cert_url}")
+    String auth_provider_x509_cert_url;
+
+    @Value("${firebase.client_x509_cert_url}")
+    String client_x509_cert_url;
+
+    @Value("${firebase.universe_domain}")
+    String universe_domain;
 
     @Bean
     FirebaseMessaging firebaseMessaging() throws IOException {
-        ClassPathResource resource = new ClassPathResource(FCM_PRIVATE_KEY_PATH);
-
-        InputStream refreshToken = resource.getInputStream();
+        InputStream refreshToken = getFirebaseRefreshToken();
 
         FirebaseApp firebaseApp = null;
         List<FirebaseApp> firebaseApps = FirebaseApp.getApps();
@@ -43,5 +77,25 @@ public class FCMConfig {
         }
 
         return FirebaseMessaging.getInstance();
+    }
+
+    private InputStream getFirebaseRefreshToken() throws IOException {
+        Map<String, String> map = new HashMap<>();
+
+        map.put("type", type);
+        map.put("project_id", project_id);
+        map.put("private_key_id", private_key_id);
+        map.put("private_key", private_key);
+        map.put("client_email", client_email);
+        map.put("client_id", client_id);
+        map.put("auth_uri", auth_uri);
+        map.put("token_uri", token_uri);
+        map.put("auth_provider_x509_cert_url", auth_provider_x509_cert_url);
+        map.put("client_x509_cert_url", client_x509_cert_url);
+        map.put("universe_domain", universe_domain);
+
+        JSONObject jsonObject = new JSONObject(map);
+        String jsonString = jsonObject.toString();
+        return new ByteArrayInputStream(jsonString.getBytes(StandardCharsets.UTF_8));
     }
 }
