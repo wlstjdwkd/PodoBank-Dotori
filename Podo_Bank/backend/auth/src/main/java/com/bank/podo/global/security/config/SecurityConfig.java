40000c4151113dc1e54b96db9e4e5521a23b3a8d
서현덕 <kkang13277@naver.com>
2023-10-03 19:34:09 +0900

    #S09P22D107-383 #in-progress #Fix(podobank) : 로그 확인을 위한 코드 추가

--- Podo_Bank/backend/auth/src/main/java/com/bank/podo/global/security/config/SecurityConfig.java
+++ Podo_Bank/backend/auth/src/main/java/com/bank/podo/global/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.bank.podo.global.security.config;
 
+import com.bank.podo.global.security.filter.JwtAuthFilter;
+import com.bank.podo.global.security.service.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -10,12 +12,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Slf4j
 @EnableWebSecurity  // 필터를 스프링 필터 체인에 등록
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtProvider jwtProvider;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -32,6 +37,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests()
                 .anyRequest().permitAll()
                 .and()
+                .addFilterBefore(new JwtAuthFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .logout().disable();
---
+++ Podo_Bank/backend/auth/src/main/java/com/bank/podo/global/security/filter/JwtAuthFilter.java
@@ -0,0 +1,47 @@
+package com.bank.podo.global.security.filter;
+
+import com.bank.podo.global.security.service.JwtProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtProvider jwtProvider;
+
+    private final String HEADER_KEY = "Authorization";
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info(request.getRequestURI()+" "+request.getMethod());
+        // body 데이터 로그 출력
+        log.info("body: "+request.getReader().lines().reduce("", (accumulator, actual) -> accumulator + actual));
+
+        String userEmail = request.getHeader("userEmail");
+
+        if (userEmail != null) {
+            // 인증 정보가 존재할 때의 처리
+            Authentication authentication = jwtProvider.getAuthentication(userEmail);
+            if(authentication == null) {
+                log.info("authentication is null");
+                return;
+            }
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            log.info(userEmail);
+        } else {
+            // 인증 정보가 존재하지 않을 때의 처리
+            log.info("authentication is null");
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
