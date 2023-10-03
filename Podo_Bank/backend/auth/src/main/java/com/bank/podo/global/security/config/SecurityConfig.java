a2fe0743c2b1058cf4a90fc9ef7ca69a6301ba3a
서현덕 <kkang13277@naver.com>
2023-10-03 19:45:51 +0900

    #S09P22D107-383 #in-progress #Fix(podobank) : 로그 확인을 위한 코드 추가 롤백

--- Podo_Bank/backend/auth/src/main/java/com/bank/podo/global/security/config/SecurityConfig.java
+++ Podo_Bank/backend/auth/src/main/java/com/bank/podo/global/security/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.bank.podo.global.security.config;
 
-import com.bank.podo.global.security.filter.JwtAuthFilter;
-import com.bank.podo.global.security.service.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +10,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Slf4j
 @EnableWebSecurity  // 필터를 스프링 필터 체인에 등록
