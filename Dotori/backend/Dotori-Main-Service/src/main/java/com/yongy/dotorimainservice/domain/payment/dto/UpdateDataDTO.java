6a80257d2d7ec78f4e557fd41cb7b262a302333e
tlalswn23 <tlalswn23@daum.net>
2023-10-04 20:38:14 +0900

    #S09P22D107- #in-progress #comment 442 feat(payment) : 미분류 수정

--- Dotori/backend/Dotori-Main-Service/src/main/java/com/yongy/dotorimainservice/domain/payment/dto/UpdateDataDTO.java
+++ Dotori/backend/Dotori-Main-Service/src/main/java/com/yongy/dotorimainservice/domain/payment/dto/UpdateDataDTO.java
@@ -1,8 +1,12 @@
 package com.yongy.dotorimainservice.domain.payment.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
+@AllArgsConstructor
 public class UpdateDataDTO {
     Long planDetailSeq;
     Long paymentSeq;
