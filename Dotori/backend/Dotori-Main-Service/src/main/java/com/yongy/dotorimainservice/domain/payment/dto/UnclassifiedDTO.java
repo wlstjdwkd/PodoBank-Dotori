c8883f2b4813bbce47df51abbe842d9515b6b93d
tlalswn23 <tlalswn23@daum.net>
2023-10-04 20:40:27 +0900

    #S09P22D107- #in-progress #comment 442 feat(payment) : 미분류 수정

--- Dotori/backend/Dotori-Main-Service/src/main/java/com/yongy/dotorimainservice/domain/payment/dto/UnclassifiedDTO.java
+++ Dotori/backend/Dotori-Main-Service/src/main/java/com/yongy/dotorimainservice/domain/payment/dto/UnclassifiedDTO.java
@@ -4,12 +4,14 @@ import com.yongy.dotorimainservice.domain.payment.dto.UpdateDataDTO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class UnclassifiedDTO {
     List<UpdateDataDTO> updateData;
 }
--- Dotori/backend/Dotori-Main-Service/src/main/java/com/yongy/dotorimainservice/domain/payment/dto/UpdateDataDTO.java
+++ Dotori/backend/Dotori-Main-Service/src/main/java/com/yongy/dotorimainservice/domain/payment/dto/UpdateDataDTO.java
@@ -3,10 +3,12 @@ package com.yongy.dotorimainservice.domain.payment.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class UpdateDataDTO {
     Long planDetailSeq;
     Long paymentSeq;
