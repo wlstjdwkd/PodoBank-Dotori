7f5dee131d251ad487baf097b801643de11c7290
tlalswn23 <tlalswn23@daum.net>
2023-10-05 03:51:43 +0900

    #S09P22D107- #in-progress #comment 442 feat(reward) : randomBox 송금 url 변경

--- Dotori/backend/Dotori-Main-Service/src/main/java/com/yongy/dotorimainservice/domain/bank/controller/BankController.java
+++ Dotori/backend/Dotori-Main-Service/src/main/java/com/yongy/dotorimainservice/domain/bank/controller/BankController.java
@@ -27,9 +27,9 @@ public class BankController {
     }
 
     @GetMapping("/communication/bankInfo")
-    public ResponseEntity<BankDto> getBankInfo(@RequestParam Long bankSeq){
+    public ResponseEntity<BankDto> getBankInfo(@RequestParam String bankSeq){
         log.info("come - BANK");
-        return ResponseEntity.ok(bankService.bankDtoFromBank(bankSeq));
+        return ResponseEntity.ok(bankService.bankDtoFromBank(Long.valueOf(bankSeq)));
     }
 
 }
