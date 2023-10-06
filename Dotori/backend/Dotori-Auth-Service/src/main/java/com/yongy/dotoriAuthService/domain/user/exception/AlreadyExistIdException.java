4dc0d86bba710d1a7844fe083c972e43a8d82479
Poposong <psi1908@naver.com>
2023-10-06 11:12:58 +0900

    #done #comment feat(DotoriAuthService) : MSA 합치기

--- Dotori/backend/Dotori-Auth-Service/src/main/java/com/yongy/dotoriAuthService/domain/user/exception/AlreadyExistIdException.java
+++
@@ -1,7 +0,0 @@
-package com.yongy.dotoriAuthService.domain.user.exception;
-
-public class AlreadyExistIdException extends RuntimeException{
-    public AlreadyExistIdException(String message){
-        super(message);
-    }
-}
--- Dotori/backend/Dotori-Auth-Service/src/main/java/com/yongy/dotoriAuthService/domain/user/exception/FailedSocialAuthException.java
+++
@@ -1,7 +0,0 @@
-package com.yongy.dotoriAuthService.domain.user.exception;
-
-public class FailedSocialAuthException extends RuntimeException{
-    public FailedSocialAuthException(String message){
-        super(message);
-    }
-}
